61cb81ff73627614bf760456addbee34a1c485c5
jpromanonet <programming@jpromano.net>
2021-07-20 10:21:09 -0300

    Work data

--- src/data/work.js
+++ src/data/work.js
@@ -1,11 +1,20 @@
 export const WorkData = [
   {
-    company: "Company 1",
+    company: "Matelab",
     designation: "Software Engineer",
-    dateJoining: "02-02-2000",
-    dateEnd: "Present",
+    dateJoining: "11/2019 ",
+    dateEnd: " Present",
     companyLogo:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png",
+    work: "Worken on Unit testing and creating the largest database efficiently. With over 100 people under me, made it work really well.",
+  },
+  {
+    company: "Globant",
+    designation: "DevOps Engineer",
+    dateJoining: "09/2020 ",
+    dateEnd: " 04/2021",
+    companyLogo:
+      "https://iconape.com/wp-content/png_logo_vector/facebook-f-logo-2019.png",
     work: "Worken on Unit testing and creating the largest database efficiently. With over 100 people under me, made it work really well.",
   },
   {
