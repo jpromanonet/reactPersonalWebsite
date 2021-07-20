92271e7a7d83fe4736fcb08c3335cdccc60d60ab
jpromanonet <programming@jpromano.net>
2021-07-20 12:08:28 -0300

    Work data

--- src/data/work.js
+++ src/data/work.js
@@ -18,10 +18,10 @@ export const WorkData = [
     work: "Worken on Unit testing and creating the largest database efficiently. With over 100 people under me, made it work really well.",
   },
   {
-    company: "Company 2",
-    designation: "Software Engineer",
-    dateJoining: "02-02-2000",
-    dateEnd: "Present",
+    company: "El Cronista Comercial",
+    designation: "Full Stack Developer",
+    dateJoining: "09/2018",
+    dateEnd: "09/2020",
     companyLogo:
       "https://iconape.com/wp-content/png_logo_vector/facebook-f-logo-2019.png",
     work: "Worken on Unit testing and creating the largest database efficiently. With over 100 people under me, made it work really well.",
