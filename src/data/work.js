a5e2117c142ecde5b966ac072c0f9836c7328b69
jpromanonet <programming@jpromano.net>
2021-07-21 21:25:39 -0300

    Agrego accusys

--- src/data/work.js
+++ src/data/work.js
@@ -44,4 +44,13 @@ export const WorkData = [
       "https://iconape.com/wp-content/png_logo_vector/facebook-f-logo-2019.png",
     work: "IT support level 2 and sometimes 3, internal software development and virtualization platforms administration like VMWare and ProxMox, linux servers, etc.",
   },
+  {
+    company: "AG S.A",
+    designation: "Full Stack Developer",
+    dateJoining: "11/2012 ",
+    dateEnd: " 12/2015",
+    companyLogo:
+      "https://iconape.com/wp-content/png_logo_vector/facebook-f-logo-2019.png",
+    work: "IT support level 2 and sometimes 3, internal software development and virtualization platforms administration like VMWare and ProxMox, linux servers, etc.",
+  },
 ];
