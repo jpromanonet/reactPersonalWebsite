8b89f2c66cee202682cc34e20be8e34eafd04b56
jpromanonet <programming@jpromano.net>
2021-07-25 20:35:37 -0300

    Education

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 function Footer() {
-  return <div className="footer">Made with 🔥 by Overreacted</div>;
+  return <div className="footer">Made with ❤️</div>;
 }
 
 export default Footer;
