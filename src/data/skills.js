f5f40a191756658034b90ceda984ebaf37081c1d
jpromanonet <programming@jpromano.net>
2021-07-27 23:39:00 -0300

    Skills icons

--- src/data/skills.js
+++ src/data/skills.js
@@ -24,8 +24,32 @@ export const SkillsData = [
     type: "Backend",
     list: [
       {
+        name: "C",
+        icon: <i class="devicon-c-plain colored"></i>,
+      },
+      {
         name: "C++",
         icon: <i class="devicon-cplusplus-plain colored"></i>,
+      },
+      {
+        name: "C#",
+        icon: <i class="devicon-csharp-plain colored"></i>,
+      },
+      {
+        name: "Java",
+        icon: <i class="devicon-java-plain colored"></i>,
+      },
+      {
+        name: ".Net",
+        icon: <i class="devicon-microsoft-plain colored"></i>,
+      },
+      {
+        name: "PHP",
+        icon: <i class="devicon-php-plain colored"></i>,
+      },
+      {
+        name: "Python",
+        icon: <i class="devicon-python-plain colored"></i>,
       },
       {
         name: "NodeJS",
