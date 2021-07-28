2004e8ad2c573254ea8a02e9b522e14f593eb4e9
jpromanonet <programming@jpromano.net>
2021-07-28 13:07:37 -0300

    Skills icons

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,34 +1,5 @@
 export const SkillsData = [
   {
-    type: "Frontend",
-    list: [
-      {
-        name: "ReactJS",
-        icon: <i class="devicon-react-original colored"></i>,
-      },
-      {
-        name: "Javascript",
-        icon: <i class="devicon-javascript-plain colored"></i>,
-      },
-      {
-        name: "VueJS",
-        icon: <i class="devicon-vuejs-plain colored"></i>,
-      },
-      {
-        name: "CSS3",
-        icon: <i class="devicon-css3-plain colored"></i>,
-      },
-      {
-        name: "HTML5",
-        icon: <i class="devicon-html5-plain colored"></i>,
-      },
-      {
-        name: "Bootstrap",
-        icon: <i class="devicon-bootstrap-plain colored"></i>,
-      },
-    ],
-  },
-  {
     type: "Backend",
     list: [
       {
@@ -70,6 +41,56 @@ export const SkillsData = [
       {
         name: "Matlab",
         icon: <i class="devicon-matlab-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "ReactJS",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "Javascript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "VueJS",
+        icon: <i class="devicon-vuejs-plain colored"></i>,
+      },
+      {
+        name: "CSS3",
+        icon: <i class="devicon-css3-plain colored"></i>,
+      },
+      {
+        name: "HTML5",
+        icon: <i class="devicon-html5-plain colored"></i>,
+      },
+      {
+        name: "Bootstrap",
+        icon: <i class="devicon-bootstrap-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Databases",
+    list: [
+      {
+        name: "MySQL",
+        icon: <i class="devicon-mysql-plain colored"></i>,
+      },
+      {
+        name: "Docker",
+        icon: <i class="devicon-microsoftsqlserver-plain colored"></i>,
+      },
+      {
+        name: "Bash",
+        icon: <i class="devicon-bash-plain colored"></i>,
+      },
+      {
+        name: "Linux",
+        icon: <i class="devicon-linux-plain colored"></i>,
       },
     ],
   },
