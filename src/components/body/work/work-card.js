cde41aaf32b52b86298a5315bdd723e981039cf5
jpromanonet <programming@jpromano.net>
2021-08-01 18:43:33 -0300

    Skills update

--- src/components/body/work/work-card.js
+++ src/components/body/work/work-card.js
@@ -5,7 +5,7 @@ function WorkCard({ item }) {
     <div className="work-card">
       <img src={item.companyLogo} className="work-logo" />
       <div className="work-info">
-        <label className="company-name">{item.company}</label>
+        <label className="company-name">{item.designation} at {item.company}</label>
         <div className="work-dates">
           <label>{item.dateJoining}</label>-<label>{item.dateEnd}</label>
         </div>
