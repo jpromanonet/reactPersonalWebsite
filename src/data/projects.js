e0496a5349ac49591713cbdd0db12cd1454b9d6e
jpromanonet <programming@jpromano.net>
2022-01-13 14:40:12 -0300

    Netflix Clone

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,7 +13,7 @@ export const ProjectData = [
 
   {
     id: 1,
-    title: "Random Project 1",
+    title: "Instagram Clone",
     about:
       "This is a random project I am using to illustrate in my video. This is channel overreacted and I hope you are enjoying this video. Make sure you like and comment if you have any doubt.",
     tags: [
