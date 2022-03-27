e4765758e94526f1cf663a3cdc012c8ed01f7276
jpromanonet <programming@jpromano.net>
2022-03-26 22:52:06 -0300

    Cleaning up projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,40 +10,4 @@ export const ProjectData = [
     image:
       "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg",
   },
-  {
-    id: 3,
-    title: "Instagram Clone",
-    about:
-      "This is a Instagram clone using the MERN stack.",
-    tags: [
-      "ReactJs",
-      "NodeJs",
-      "Firebase",
-      "NextJS",
-      "ReactJS",
-      "NodeJs",
-      "Firebase",
-    ],
-    demo: "www.example.com",
-    github: "www.example.com",
-    image: "https://p4.wallpaperbetter.com/wallpaper/948/460/1018/technology-instagram-social-media-hd-wallpaper-preview.jpg"
-  },
-  {
-    id: 3,
-    title: "Amazon Clone (Amazona)",
-    about:
-      "This is a Amazon clone using the MERN stack.",
-    tags: [
-      "ReactJs",
-      "NodeJs",
-      "Firebase",
-      "NextJS",
-      "ReactJS",
-      "NodeJs",
-      "Firebase",
-    ],
-    demo: "www.example.com",
-    github: "www.example.com",
-    image: "https://p4.wallpaperbetter.com/wallpaper/948/460/1018/technology-instagram-social-media-hd-wallpaper-preview.jpg"
-  },
 ];
