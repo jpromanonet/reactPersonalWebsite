08666230028d762074d1effe11f0ed1e47013079
jpromanonet <programming@jpromano.net>
2022-03-28 23:54:42 -0300

    new build

--- src/data/skills.js
+++ src/data/skills.js
@@ -27,6 +27,26 @@ export const SkillsData = [
         icon: <i class="devicon-codeigniter-plain colored"></i>,
       },
       {
+        name: "Laravel",
+        icon: <i class="devicon-laravel-plain colored"></i>,
+      },
+      {
+        name: "Flask",
+        icon: <i class="devicon-flask-plain colored"></i>,
+      },
+      {
+        name: "Golang",
+        icon: <i class="devicon-go-plain colored"></i>,
+      },
+      {
+        name: "Rust",
+        icon: <i class="devicon-rust-plain colored"></i>,
+      },
+      {
+        name: "Django",
+        icon: <i class="devicon-django-plain colored"></i>,
+      },
+      {
         name: "Python",
         icon: <i class="devicon-python-plain colored"></i>,
       },
@@ -38,6 +58,34 @@ export const SkillsData = [
         name: "R",
         icon: <i class="devicon-r-plain colored"></i>,
       },
+      {
+        name: "Perl",
+        icon: <i class="devicon-perl-plain colored"></i>,
+      },
+      {
+        name: "Lisp",
+        icon: <i class="devicon-ocaml-plain colored"></i>,
+      },
+      {
+        name: "PyCharm",
+        icon: <i class="devicon-pycharm-plain colored"></i>,
+      },
+      {
+        name: "Rails",
+        icon: <i class="devicon-rails-plain colored"></i>,
+      },
+      {
+        name: "Ruby",
+        icon: <i class="devicon-ruby-plain colored"></i>,
+      },
+      {
+        name: ".Net",
+        icon: <i class="devicon-dot-net-plain colored"></i>,
+      },
+      {
+        name: ".Net Core",
+        icon: <i class="devicon-dotnetcore-plain colored"></i>,
+      },
     ],
   },
   {
@@ -48,8 +96,24 @@ export const SkillsData = [
         icon: <i class="devicon-git-plain colored"></i>,
       },
       {
+        name: "Github",
+        icon: <i class="devicon-github-plain colored"></i>,
+      },
+      {
+        name: "Gitlab",
+        icon: <i class="devicon-gitlab-plain colored"></i>,
+      },
+      {
         name: "Docker",
         icon: <i class="devicon-docker-plain colored"></i>,
+      },
+      {
+        name: "Kubernetes",
+        icon: <i class="devicon-kubernetes-plain colored"></i>,
+      },
+      {
+        name: "Composer",
+        icon: <i class="devicon-composer-plain colored"></i>,
       },
       {
         name: "Bash",
@@ -75,21 +139,46 @@ export const SkillsData = [
         name: "Apache",
         icon: <i class="devicon-apache-plain colored"></i>,
       },
+      {
+        name: "Nginx",
+        icon: <i class="devicon-nginx-plain colored"></i>,
+      },
+      {
+        name: "Yarn",
+        icon: <i class="devicon-yarn-plain colored"></i>,
+      },
     ],
   },
   {
     type: "Frontend",
     list: [
       {
-        name: "ReactJS",
-        icon: <i class="devicon-react-original colored"></i>,
-      },
-      {
         name: "Javascript",
         icon: <i class="devicon-javascript-plain colored"></i>,
       },
       {
-        name: "VueJS",
+        name: "Typescript",
+        icon: <i class="devicon-typescript-plain colored"></i>,
+      },
+      {
+        name: "React JS",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "Redux",
+        icon: <i class="devicon-redux-plain colored"></i>,
+      },
+      {
+        name: "Next JS",
+        icon: <i class="devicon-nextjs-plain colored"></i>,
+      },
+      {
+        name: "Angular",
+        icon: <i class="devicon-angularjs-plain colored"></i>,
+      },
+      
+      {
+        name: "Vue JS",
         icon: <i class="devicon-vuejs-plain colored"></i>,
       },
       {
@@ -101,12 +190,24 @@ export const SkillsData = [
         icon: <i class="devicon-css3-plain colored"></i>,
       },
       {
+        name: "Tailwind",
+        icon: <i class="devicon-tailwindcss-plain colored"></i>,
+      },
+      {
+        name: "Babel",
+        icon: <i class="devicon-babel-plain colored"></i>,
+      },
+      {
         name: "HTML5",
         icon: <i class="devicon-html5-plain colored"></i>,
       },
       {
         name: "Bootstrap",
         icon: <i class="devicon-bootstrap-plain colored"></i>,
+      },
+      {
+        name: "Sass",
+        icon: <i class="devicon-sass-plain colored"></i>,
       },
     ],
   },
