b18ccceb63495b4ef3acc3e7a5f37f9595372aab
jpromanonet <programming@jpromano.net>
2022-04-01 18:25:27 -0300

    Tinder clone

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,4 +10,15 @@ export const ProjectData = [
     image:
       "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg",
   },
+  {
+    id: 1,
+    title: "Ultimate Tinder Clone",
+    about:
+      "A tinder clone with MongoDB and Node Js",
+    tags: ["context", "React JS", "Node JS", "CRM React"],
+    demo: "www.example.com",
+    github: "https://github.com/jpromanonet/",
+    image:
+      "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg",
+  },
 ];
