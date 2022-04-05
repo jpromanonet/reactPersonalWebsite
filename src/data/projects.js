473f724a3889b9a54b053c437eca338b202d0126
jpromanonet <programming@jpromano.net>
2022-04-05 18:35:35 -0300

    Django CRM

--- src/data/projects.js
+++ src/data/projects.js
@@ -98,4 +98,15 @@ export const ProjectData = [
     image:
       "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg",
   },
+  {
+    id: 10,
+    title: "Django CRM",
+    about:
+      "A Costumer Relationship Manager made with React and Node JS",
+    tags: ["context", "React JS", "Node JS", "CRM React"],
+    demo: "www.example.com",
+    github: "https://github.com/jpromanonet/",
+    image:
+      "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg",
+  },
 ];
