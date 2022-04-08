ac4464f72bece90aba0c823f1ab3958575a6ed5a
jpromanonet <programming@jpromano.net>
2022-04-08 16:34:00 -0300

    New project

--- src/data/projects.js
+++ src/data/projects.js
@@ -109,4 +109,16 @@ export const ProjectData = [
     image:
       "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg",
   },
+
+  {
+    id: 10,
+    title: "Travel Blog",
+    about:
+      "A Costumer Relationship Manager made with React and Node JS",
+    tags: ["context", "React JS", "Node JS", "CRM React"],
+    demo: "www.example.com",
+    github: "https://github.com/jpromanonet/",
+    image:
+      "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg",
+  },
 ];
