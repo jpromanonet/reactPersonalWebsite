6f1a6f3175f89d5a6007c347b7fa6ef744011e77
jpromanonet <programming@jpromano.net>
2022-04-11 16:36:35 -0300

    New project

--- src/data/projects.js
+++ src/data/projects.js
@@ -114,7 +114,7 @@ export const ProjectData = [
     id: 11,
     title: "Travel Blog",
     about:
-      "Travel blog with maps using NextJS, Sanity.io",
+      "Travel blog with maps using NextJS and Sanity.io",
     tags: ["context", "React JS", "Node JS", "CRM React"],
     demo: "www.example.com",
     github: "https://github.com/jpromanonet/",
