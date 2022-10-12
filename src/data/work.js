6c69640d04eaf66e8d31c741668166e1c3af9a74
jpromanonet <programming@jpromano.net>
2022-10-11 21:12:15 -0300

    Work update

--- src/data/work.js
+++ src/data/work.js
@@ -4,7 +4,7 @@ export const WorkData = [
     designation: "Software Engineer",
     dateJoining: "10/2022 ",
     dateEnd: " Present",
-    work: "Software development with C#, .Net 6, MS-SQL, Node JS, React JS and Razor",
+    work: "Software development using C#, .Net 6, MS-SQL, Node JS, React JS and Razor",
   },
   {
     company: "The Bit",
