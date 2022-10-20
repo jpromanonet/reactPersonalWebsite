8f4043eb5866f150713f0950ee994ee005dd742f
jpromanonet <programming@jpromano.net>
2022-10-19 22:18:46 -0300

    About update

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -9,7 +9,7 @@ function About() {
           Hello there 👋 and welcome!
           <br />
           <br />
-          I'm a Software Engineer, DevOps Engineer & Teacher.
+          I'm a Software Engineer, Mathematician & Teacher.
         </div>
         <div className="about-photo">
           <img
