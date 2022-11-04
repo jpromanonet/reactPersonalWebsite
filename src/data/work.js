7ac512a2cfc848668751cabbcd61064dc52970c5
jpromanonet <programming@jpromano.net>
2022-11-04 17:06:47 -0300

    Job update

--- src/data/work.js
+++ src/data/work.js
@@ -1,7 +1,7 @@
 export const WorkData = [
   {
     company: "Binit",
-    designation: "Software Engineer & Project Manager",
+    designation: "Project Manager",
     dateJoining: "04/2022 ",
     dateEnd: " Present",
     work: "Project coordination and development with .Net Core, Angular JS and Vue JS with Azure DevOps using Scrum. Software development using C#, .Net 6, MS-SQL, Node JS, React JS and Razor",
@@ -14,7 +14,7 @@ export const WorkData = [
     work: "Executive management and client relationship development.",
   },
   {
-    company: "Andreani",
+    company: "Andreani Grupo Logistico",
     designation: "Chief of Software Development",
     dateJoining: "07/2022 ",
     dateEnd: " 10/2022",
@@ -37,7 +37,7 @@ export const WorkData = [
   {
     company: "El Cronista Comercial",
     designation: "Full Stack Developer",
-    dateJoining: "09/2018 ",
+    dateJoining: "09/2017 ",
     dateEnd: " 09/2020",
     work: "Linux servers implementation and ITIL servicies, SQL development of store procedures and report, financial systems analytics, automation of business workflows, test and software development for the company",
   },
@@ -49,7 +49,7 @@ export const WorkData = [
     work: "IT support level 2 and sometimes 3, internal software development and virtualization platforms administration like VMWare and ProxMox, linux servers, etc.",
   },
   {
-    company: "AG S.A",
+    company: "Aniceto Gómez S.A",
     designation: "Full Stack Developer",
     dateJoining: "11/2012 ",
     dateEnd: " 12/2015",
