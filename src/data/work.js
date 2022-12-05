643339e199d11eb5e87537e015f2f85317805201
jpromanonet <programming@jpromano.net>
2022-12-05 13:13:58 -0300

    Job update

--- src/data/work.js
+++ src/data/work.js
@@ -4,7 +4,7 @@ export const WorkData = [
     designation: "Director of Software Development",
     dateJoining: "04/2022 ",
     dateEnd: " Present",
-    work: "I was in charge of the developers and architecs in the company (almost 200 people) and worked in tandem with C-Level stakeholders to bring innovation to the company.",
+    work: 'Currently leading the developers and architects in the company (almost 200 people) while working in tandem with C-Level stakeholders to bring innovation to the company.'
   },
   {
     company: "Coderhouse",
@@ -39,7 +39,7 @@ export const WorkData = [
     designation: "CTO",
     dateJoining: "01/2021 ",
     dateEnd: " 04/2022",
-    work: "I coordinate and monitor the implementation of the different projects in the company on web3 with Solidity.",
+    work: "Coordination and implementation of the different projects in the company on web3 with Solidity.",
   },
   {
     company: "The Bit",
